61693d41dfa3f3b374bf00de699c3b07b7124480
Meagan Smith (she/her) <80067572+meagan13@users.noreply.github.com>
2022-06-29 11:09:55 -0500

    Update workbox-config.js file folder from froov to temp

--- workbox-config.js
+++ workbox-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-	globDirectory: 'froov',
+	globDirectory: 'temp',
 	globPatterns: [
 		'**/*.{jpg,html,jpeg,md,json,css}'
 	],
-	swDest: 'froov/sw.js',
+	swDest: 'temp/sw.js',
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
